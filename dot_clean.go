0a3ef2b3d9686986fcb3c43ec359493db82296e0
Joshua Stauter <josh.stauter@gmail.com>
2024-08-01 14:05:50 -0400

    Tested on Windows

--- dot_clean.go
+++ dot_clean.go
@@ -14,12 +14,37 @@ func main() {
 		log.Fatal("Usage: dot_clean [-m] [dir]")
 	}
 
-	filepath.Walk(filepath.Clean(os.Args[2]), func(path string, info fs.FileInfo, err error) error {
-		filename := path[strings.LastIndex(path, "/")+1:]
+	abs, err := filepath.Abs(os.Args[2])
+	if err != nil {
+		log.Fatal(err)
+	}
 
-		if strings.HasPrefix(filename, "._") {
-			fmt.Println(path)
+	// volumeName := filepath.VolumeName(abs)
+	// isWindows := volumeName != ""
+	// isWindows := runtime.GOOS == "windows"
+	// fmt.Println(isWindows)
+
+	stat, err := os.Stat(abs)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if !stat.IsDir() {
+		log.Fatal(abs + " is not a directory")
+	}
+
+	filepath.Walk(abs, func(path string, info fs.FileInfo, err error) error {
+		if !strings.HasPrefix(path[strings.LastIndex(path, string(os.PathSeparator))+1:], "._"){
+			return nil
 		}
+
+		fmt.Println("Would remove "+path)
+
+		// if err := os.Remove(path); err != nil {
+		// 	return err
+		// } else {
+		// 	fmt.Println("Removed "+path)
+		// }
 
 		return nil
 	})
